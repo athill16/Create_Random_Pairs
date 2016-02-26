f739f4a1bf73c8eb86aeaf6c47de98bd4e3602c7
Aaron Hill <aaronthill16@gmail.com>
2016-02-26 14:52:35 -0500

    Displays actual groups

--- Random_Pairs.rb
+++ Random_Pairs.rb
@@ -11,13 +11,18 @@ def create_random_pairs(array_of_students)
 		array_of_students.delete(person_two)
 		first_student_pair = [person_one, person_two]
 		group_of_pairs.push(first_student_pair)
-		if array_of_students.count == 1
-			last_person = array_of_students.sample
-			group_of_pairs[2].push(last_person)
-		end
+			if array_of_students.count == 1
+				last_person = array_of_students.sample
+				group_of_pairs.last.push(last_person)
+			end
 		end
 		group_of_pairs
 	end
 
 end
 
+array_of_pairs = create_random_pairs(["Jennifer", "Shane", "Jenny", "Meg", "Aaron", "John", "John R."])
+
+array_of_pairs.each do |group|
+	puts group.to_s
+end
