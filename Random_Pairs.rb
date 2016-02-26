02522dc49d34dac75e8e7be8a6c22d3f92c4dbd3
Aaron Hill <aaronthill16@gmail.com>
2016-02-25 20:38:11 -0500

    added test

--- Random_Pairs.rb
+++ Random_Pairs.rb
@@ -1,3 +1,13 @@
 def create_random_pairs(array_of_students)
-	group_array = [array_of_students]
+
+	if array_of_students.count <= 2
+		group_array = [array_of_students]
+	else 
+		person_one = array_of_students.sample
+		array_of_students.delete(person_one)
+		person_two = array_of_students.sample
+		array_of_students.delete(person_two)
+		student_pairs = [person_one, person_two]
+	end
+
 end
--- Random_Pairs_Tests.rb
+++ Random_Pairs_Tests.rb
@@ -10,6 +10,12 @@ class TestRandomPairs < Minitest::Test
 	def test_2_students_returns_single_pair
 		array_of_pairs = create_random_pairs(["Jennifer", "Shane"])
 		assert_equal(1, array_of_pairs.count)
+		assert_equal(2, array_of_pairs[0].count)
+	end
+
+	def test_4_students_returns_two_pairs
+		array_of_pairs = create_random_pairs(["Jennifer", "Shane", "Jenny", "Meg"])
+		assert_equal(2, array_of_pairs.count)
 	end
 
 end
