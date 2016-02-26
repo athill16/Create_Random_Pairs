f0cdb122407cea1f0d7e1f32dae7056435dc609c
Aaron Hill <aaronthill16@gmail.com>
2016-02-25 19:53:21 -0500

    Added two tests

---
+++ Random_Pairs.rb
@@ -0,0 +1,3 @@
+def create_random_pairs(array_of_students)
+	group_array = [array_of_students]
+end
--- Random_Pairs_Tests.rb
+++ Random_Pairs_Tests.rb
@@ -3,5 +3,13 @@ require_relative "Random_Pairs.rb"
 
 class TestRandomPairs < Minitest::Test
 
-	def test_
-		
+	def test_0_students_returns_empty_multidimensional_array
+		assert_equal([[]], create_random_pairs([]))
+	end
+
+	def test_2_students_returns_single_pair
+		array_of_pairs = create_random_pairs(["Jennifer", "Shane"])
+		assert_equal(1, array_of_pairs.count)
+	end
+
+end
